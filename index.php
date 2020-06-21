8a8a65dab8c48353b3714a790f765cbd63fe2a68
Donny <donnyfardhani@gmail.com>
2020-06-21 20:56:39 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Coba git remote 2</title>
+</head>
+<body>
+    <p>Coba push local</p>
+</body>
+</html>
